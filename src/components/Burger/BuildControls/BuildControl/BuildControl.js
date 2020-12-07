eb5fc5cac0da3582d928646ccd52044ff0816ca4
Francesco Maida <francesco.maida@nttdata.com>
2020-12-07 18:03:00 +0100

    changed gitignore

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,15 @@
+import classes from './BuildControl.module.css'
+
+const buildControl = (props) => (
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button className={classes.Less} onClick={props.removed} disabled={props.disabled}>
+      less
+    </button>
+    <button className={classes.More} onClick={props.added}>
+      more
+    </button>
+  </div>
+)
+
+export default buildControl
---
+++ src/components/UI/Modal/Modal.js
@@ -0,0 +1,35 @@
+import { Component } from 'react'
+
+import classes from './Modal.module.css'
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
+import Backdrop from '../Backdrop/Backdrop'
+
+class Modal extends Component {
+  // NOTE: Checks if the the state of the BurgerBuild has changed in order to prevent render
+  // when only Ingredients are changed -> it also prevents render on OrderSummary (Which is a wrapped by Modal)
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return nextProps.show !== this.props.show
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop
+          show={this.props.show}
+          clicked={this.props.modalClosed}
+        ></Backdrop>
+        <div
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0',
+          }}
+          className={classes.Modal}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    )
+  }
+}
+
+export default Modal
